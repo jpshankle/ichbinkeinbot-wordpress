e75c53bd3d7930dabf515d3a07cbee46ee2a3510
Jeremy Shankle <jpshankle@gmail.com>
2021-01-28 11:26:04 -0500

    bitcoin

--- ich-bin-kein-bot.php
+++ ich-bin-kein-bot.php
@@ -7,6 +7,7 @@
  */
 
 $ichbin = 'wer';
+$bitcoin_price = 'btc';
 
 add_shortcode(
     $ichbin,
@@ -19,4 +20,17 @@ function wer_func() {
     $werReturn .= '</p>';
     
     return $werReturn;
+} 
+
+add_shortcode(
+  $bitcoin_price,
+  'get_bitcoin_price'
+);
+
+function get_bitcoin_price() {
+  $btc_price_response = wp_remote_get('https://api.coinbase.com/v2/prices/spot?currency=USD');
+  $btc_price_body = json_decode(wp_remote_retrieve_body($btc_price_response), true);
+  $current_price_usd = '$' . $btc_price_body['data']['amount'];
+
+  return $current_price_usd;
 }
